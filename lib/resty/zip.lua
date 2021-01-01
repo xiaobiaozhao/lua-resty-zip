7ab7dfed3a0401becaf21613432cb98dc053ff41
zhaoxiaobiao <zhaoxiaobiaogm@gmail.com>
2021-01-01 23:56:32 +0800

    兼容mac

--- lib/resty/zip.lua
+++ lib/resty/zip.lua
@@ -3,7 +3,7 @@
 local ffi = require "ffi"
 local ffi_new = ffi.new
 local ffi_str = ffi.string
-local C = ffi.C
+local zip = ffi.C
 
 local _M = {_VERSION = "0.02"}
 
@@ -15,13 +15,17 @@ ffi.cdef [[
         const uint8_t *source, unsigned long sourceLen);
 ]]
 
+if ffi.os == "OSX" then
+    zip = ffi.load("z")
+end
+
 -- level: 0 - 9
 function _M.compress(txt, level)
-    local n = C.compressBound(#txt)
+    local n = zip.compressBound(#txt)
     local buf = ffi_new("uint8_t[?]", n)
 
     local buflen = ffi_new("unsigned long[1]", n)
-    local res = C.compress2(buf, buflen, txt, #txt, level or 1)
+    local res = zip.compress2(buf, buflen, txt, #txt, level or 1)
 
     if res == 0 then
         return ffi_str(buf, buflen[0])
@@ -32,7 +36,7 @@ function _M.uncompress(comp, n)
     local buf = ffi_new("uint8_t[?]", n)
 
     local buflen = ffi_new("unsigned long[1]", n)
-    local res = C.uncompress(buf, buflen, comp, #comp)
+    local res = zip.uncompress(buf, buflen, comp, #comp)
 
     if res == 0 then
         return ffi_str(buf, buflen[0])
